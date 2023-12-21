0d8bdb04c51c3f8e15e88a4e9b8e31bb88117284
Leo Hsu <leodream251324@gmail.com>
2023-12-21 21:37:01 +0800

    change toLocaleString to DataTimeFormat

--- backend/utils/toCorrectString.js
+++ backend/utils/toCorrectString.js
@@ -1,16 +1,20 @@
 
 const toCorrectString = (date) => {
     const targetTimeZone = 'Asia/Taipei';
-    const localeString = date.toLocaleString('en-US', {
-    timeZone: targetTimeZone,
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
-    hour12: false, 
+
+    const formatter = Intl.DateTimeFormat('en-US', {
+        timeZone: targetTimeZone,
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false, 
     });
+
+    const localeString = formatter.format(date);
+
     const [datePart, timePart] = localeString.split(', ');
     const [month, day, year] = datePart.split('/');
     const [hours, minutes, seconds] = timePart.split(':');
